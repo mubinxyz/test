775e4ff578f93c4b797e72c29b31176d7f0ecd6b
mubinxyz <mubintp2003@gmail.com>
2024-02-06 13:35:24 +0330

    fix bugs and finished admin route

--- js/node/full-todo/src/api/routes/v1/index.js
+++ js/node/full-todo/src/api/routes/v1/index.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 
-const usersRoute = require("./user/users");
+const adminRoute = require("./admin/admin");
 const todosRoute = require("./todo/todos");
 const authRoute = require("./auth/auth");
 
 const v1Loader = () => {
   const router = Router();
-  router.use("/users", usersRoute);
+  router.use("/admin", adminRoute);
   router.use("/todos", todosRoute);
   router.use("/auth", authRoute);
   return router;
